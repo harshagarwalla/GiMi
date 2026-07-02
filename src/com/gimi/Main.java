c837b07dfb1aacc0a6bc5565963758a6a7e5b16e
Harsh Agarwalla <pappua088@gmail.com>
2026-07-02 19:30:25 +0530

    Set up project structure and added welcome screen

---
+++ src/com/gimi/Main.java
@@ -0,0 +1,17 @@
+package com.gimi;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("=========================");
+        System.out.println("         GiMi v0.1       ");
+        System.out.println("=========================");
+        System.out.println();
+        System.out.println("Hello!");
+        System.out.println();
+        System.out.println("I am GiMi.");
+        System.out.println("Your Personal Assistant.");
+        System.out.println();
+        System.out.println("Type \"help\" to see available commands.");
+    }
+}
